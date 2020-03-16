80fdbb6155d6210dafca5fc2c64e38fd64c00eb7
Mary Paz Aguilar <marypaz-aghe@hotmail.com>
2020-03-16 01:31:10 -0600

    [Updated]: Edit line bar chart now with results

--- lib/lineBar.dart
+++ lib/lineBar.dart
@@ -3,6 +3,8 @@
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
 
+
+
 class lineBar extends StatelessWidget {
   final List<charts.Series> seriesList;
   final bool animate;
@@ -14,7 +16,7 @@ class lineBar extends StatelessWidget {
     return new lineBar(
       _createSampleData(),
       // Disable animations for image tests.
-      animate: false,
+      animate: true,
     );
   }
 
@@ -31,7 +33,11 @@ class lineBar extends StatelessWidget {
           new charts.BarRendererConfig(
             // ID used to link series to this renderer.
               customRendererId: 'customBar')
-        ]);
+        ],
+        behaviors: [
+        new charts.SeriesLegend(position: charts.BehaviorPosition.end),
+    ],
+    );
     return Scaffold(
       appBar: AppBar(
         title: Text("Line bar"),
@@ -41,7 +47,7 @@ class lineBar extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           new Text(
-            'You have pushed the button this many times:',
+            'Discapacidad para oir según el genero',
           ),
           //new Text(
           //  '${clicksCount[actualClickData]}',
@@ -54,77 +60,65 @@ class lineBar extends StatelessWidget {
               child: chart,
             ),
           ),
-          FlatButton(
-            child: Icon(Icons.add),
-            color: Colors.cyan,
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),
-            // onPressed: (){
-            // setState(() {
-            // ++clicksCount[actualClickData];
-            // });
-            // },
-          )
+
         ],
       ),
     );
   }
 
   /// Create one series with sample hard coded data.
-  static List<charts.Series<LinearSales, int>> _createSampleData() {
-    final desktopSalesData = [
-      new LinearSales(0, 5),
-      new LinearSales(1, 25),
-      new LinearSales(2, 100),
-      new LinearSales(3, 75),
+  static List<charts.Series<discapacityData, int>> _createSampleData() {
+    final womenData = [
+      new discapacityData(9, 579),
+      new discapacityData(19, 1561),
+      new discapacityData(29, 1533),
+      new discapacityData(39, 1997),
+      new discapacityData(49, 3404),
+      new discapacityData(59, 4515),
+      new discapacityData(69, 5192),
+      new discapacityData(79, 6160),
+      new discapacityData(99, 8428),
     ];
 
-    final tableSalesData = [
-      new LinearSales(0, 5),
-      new LinearSales(1, 25),
-      new LinearSales(2, 100),
-      new LinearSales(3, 75),
+    final menData = [
+      new discapacityData(9, 888),
+      new discapacityData(19, 1814),
+      new discapacityData(29, 1847),
+      new discapacityData(39, 2262),
+      new discapacityData(49, 3635),
+      new discapacityData(59, 4830),
+      new discapacityData(69, 6375),
+      new discapacityData(79, 7591),
+      new discapacityData(99, 7998),
     ];
 
-    final mobileSalesData = [
-      new LinearSales(0, 10),
-      new LinearSales(1, 50),
-      new LinearSales(2, 200),
-      new LinearSales(3, 150),
-    ];
+
 
     return [
-      new charts.Series<LinearSales, int>(
-        id: 'Desktop',
-        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
-        domainFn: (LinearSales sales, _) => sales.year,
-        measureFn: (LinearSales sales, _) => sales.sales,
-        data: desktopSalesData,
+      new charts.Series<discapacityData, int>(
+        id: 'Mujeres',
+        colorFn: (discapacityData_, _sales_) => charts.MaterialPalette.green.shadeDefault,
+        domainFn: (discapacityData sales, _) => sales.age,
+        measureFn: (discapacityData sales, _) => sales.discapacity,
+        data: womenData,
       )
       // Configure our custom bar renderer for this series.
         ..setAttribute(charts.rendererIdKey, 'customBar'),
-      new charts.Series<LinearSales, int>(
-        id: 'Tablet',
+      new charts.Series<discapacityData, int>(
+        id: 'Hombres',
         colorFn: (_, __) => charts.MaterialPalette.red.shadeDefault,
-        domainFn: (LinearSales sales, _) => sales.year,
-        measureFn: (LinearSales sales, _) => sales.sales,
-        data: tableSalesData,
-      )
-      // Configure our custom bar renderer for this series.
-        ..setAttribute(charts.rendererIdKey, 'customBar'),
-      new charts.Series<LinearSales, int>(
-          id: 'Mobile',
-          colorFn: (_, __) => charts.MaterialPalette.green.shadeDefault,
-          domainFn: (LinearSales sales, _) => sales.year,
-          measureFn: (LinearSales sales, _) => sales.sales,
-          data: mobileSalesData),
+        domainFn: (discapacityData sales, _) => sales.age,
+        measureFn: (discapacityData sales, _) => sales.discapacity,
+        data: menData,
+      ),
     ];
   }
 }
 
 /// Sample linear data type.
-class LinearSales {
-  final int year;
-  final int sales;
+class discapacityData {
+  final int age;
+  final int discapacity;
 
-  LinearSales(this.year, this.sales);
+  discapacityData(this.age, this.discapacity);
 }
